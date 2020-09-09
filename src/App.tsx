1e7a1c5f581c975c9893ea849d0148693266ff1f
shizuku <2112165916@qq.com>
2020-09-09 17:56:37 +0800

    add example image

--- src/App.tsx
+++ src/App.tsx
@@ -16,8 +16,8 @@ class App extends React.Component<
   }
 
   componentDidMount() {
-    fetch("https://cdn.jsdelivr.net/gh/shizuku/muse-react/test/server/b.json")
-    //fetch("http://localhost:8888/b")
+    //fetch("https://cdn.jsdelivr.net/gh/shizuku/muse-react/test/server/b.json")
+    fetch("http://localhost:8888/b")
       .then((res) => res.text())
       .then(
         (result) => {
