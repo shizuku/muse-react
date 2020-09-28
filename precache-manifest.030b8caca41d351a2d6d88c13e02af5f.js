5bae2315313aeb1db01e4f2b194d8922eea265eb
shizuku <2112165916@qq.com>
2020-09-28 14:27:45 +0800

    Updates

--- precache-manifest.030b8caca41d351a2d6d88c13e02af5f.js
+++ precache-manifest.030b8caca41d351a2d6d88c13e02af5f.js
@@ -1,23 +1,23 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "d565428a01fb10ea2ffe7debdf822587",
+    "revision": "6154dc3cab252e237601083f1bbd120d",
     "url": "/muse-react/index.html"
   },
   {
-    "revision": "345c231deb672dd001de",
+    "revision": "0f982799783e5877f713",
     "url": "/muse-react/static/css/main.06c0bf08.chunk.css"
   },
   {
-    "revision": "a15507e0607bf28f5192",
-    "url": "/muse-react/static/js/2.ce267cc8.chunk.js"
+    "revision": "c2e703f15fde1423a442",
+    "url": "/muse-react/static/js/2.3fc57aec.chunk.js"
   },
   {
     "revision": "501f379103cbfcb5ff11211dd39a28a2",
-    "url": "/muse-react/static/js/2.ce267cc8.chunk.js.LICENSE.txt"
+    "url": "/muse-react/static/js/2.3fc57aec.chunk.js.LICENSE.txt"
   },
   {
-    "revision": "345c231deb672dd001de",
-    "url": "/muse-react/static/js/main.d545dd4b.chunk.js"
+    "revision": "0f982799783e5877f713",
+    "url": "/muse-react/static/js/main.7d66b080.chunk.js"
   },
   {
     "revision": "d0b49ca1b091f73ad63d",
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/muse-react/precache-manifest.dd1cf63cd4e59ee092c0e02b763f324f.js"
+  "/muse-react/precache-manifest.030b8caca41d351a2d6d88c13e02af5f.js"
 );
 
 self.addEventListener('message', (event) => {
